04c9ffe02553dc28594f84647e993a5425a1fd83
Johan <zymxqf@hotmail.com>
2017-01-11 22:33:26 +0800

    complete css styles

---
+++ todo.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    var dropLevel = document.querySelector(".task-spread-level1");
+    var dropLevelItems = dropLevel.querySelectorAll(".task-spread-level1-item");
+    
+
+
+}

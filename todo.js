5c61e25e40daaf0ba95acf47206be11d918cb5e3
Johan <zymxqf@hotmail.com>
2017-01-14 23:54:08 +0800

    finish muti-levels dropdown menu

--- todo.js
+++ todo.js
@@ -1,7 +1,34 @@
 window.onload = function () {
-    var dropLevel = document.querySelector(".task-spread-level1");
-    var dropLevelItems = dropLevel.querySelectorAll(".task-spread-level1-item");
+    var dropLevelOne = document.querySelector("#task-sort-head");
+    EventUtil.addHandler(dropLevelOne,"click",function(event){
+        event = EventUtil.getEvent(event);
+        var target = EventUtil.getTarget(event);
+
+        if(target.classList.contains('task-spread-level1-icon')){
+            var levelTwo;
+            var lists = target.parentNode.querySelectorAll(".task-spread-level2");
+            for(var j = 0;j < lists.length;j++){
+                levelTwo = lists[j];
+                levelTwo.classList.toggle("hide");
+            }
+        }else if(target.parentNode.classList.contains("task-sort-all")){
+            var levelAll = target.parentNode.querySelector("#task-spread-all");
+            levelAll.classList.toggle("hide");
+        }else if(target.parentNode.classList.contains("task-sort-sortlist")){
+            var levelOne = target.parentNode.querySelector("#task-spread-level1");
+            levelOne.classList.toggle("hide");
+        }else if(target.parentNode.classList.contains("task-spread-level2-item")){
+            var levelTwos = document.querySelectorAll(".task-spread-level2-item");
+            var levelTwo;
+            var len = levelTwos.length;
+            for(var i = 0;i < len;i++){
+                levelTwo = levelTwos[i];
+                if(levelTwo.classList.contains("item-active")){
+                    levelTwo.classList.remove("item-active");
+                }
+            }
+            target.parentNode.classList.add("item-active");
+        }
+    });
+};
     
-
-
-}
--- util.js
+++ util.js
@@ -8,6 +8,13 @@ var EventUtil = {
             element["on"+type] = handler;
         }
     },
+    getEvent : function(event){
+        return event ? event:window.event;
+
+    },
+    getTarget : function(event){
+        return event.target || event.srcElement;
+    },
     removeHandler: function(element,type,handler){
         if(element.removeEventListener){
             element.removeEventListener(type,handler,false);
